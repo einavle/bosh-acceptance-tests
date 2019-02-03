911c5a5e517ae3484e7e6d9c945eed66c8eb0ba0
c5282127 <einav.leyboshor@sap.com>
2019-02-03 14:49:09 +0200

    remove all tests but one

--- spec/system/with_release_stemcell_deployment_spec.rb
+++ spec/system/with_release_stemcell_deployment_spec.rb
@@ -5,7 +5,7 @@ require 'system/spec_helper'
 
   describe 'ssh' do
     it 'can bosh ssh into a vm' do
-      expect(bosh_ssh('batlight', 0, "uname -a", deployment: deployment.name).output).to match /Linux/
+      expect(bosh_ssh('batlight', 1, "uname -a", deployment: deployment.name).output).to match /Linux/
     end
   end
 
